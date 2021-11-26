c954beeeb3bb685460b2389b3814088a633d9e07
Karrar Mohammed <karrar.m.habeeb@gmail.com>
2021-11-26 07:46:00 +0300

    make line chart horizontally scrollable

--- app/src/main/java/com/example/typing_tracker/ui/statistics/StatisticsFragment.kt
+++ app/src/main/java/com/example/typing_tracker/ui/statistics/StatisticsFragment.kt
@@ -4,7 +4,6 @@ package com.example.typing_tracker.ui.statistics
 import com.example.typing_tracker.R
 import com.example.typing_tracker.databinding.FragmentStatisticsBinding
 import com.example.typing_tracker.ui.base.BaseFragment
-import com.github.aachartmodel.aainfographics.aachartcreator.AAChartModel
 import com.github.aachartmodel.aainfographics.aachartcreator.AAChartType
 import com.github.aachartmodel.aainfographics.aachartcreator.AAOptions
 import com.github.aachartmodel.aainfographics.aachartcreator.AASeriesElement
@@ -21,19 +20,33 @@ class StatisticsFragment : BaseFragment<FragmentStatisticsBinding, StatisticsVie
     }
 
     override fun setUpBinding() {
-        val lineChartModel : AAChartModel = AAChartModel()
-            .chartType(AAChartType.Line)
-            .dataLabelsEnabled(true)
-            .categories(arrayOf("2/2","2/3","2/4","2/5", "2/6","2/7","2/8","2/9","2/10","2/11","2/12","2/13"))
-            .series(arrayOf(
-                AASeriesElement()
-                    .name("accuracy")
-                    .data(arrayOf(7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6)),
-                AASeriesElement()
-                    .name("WPM")
-                    .data(arrayOf(0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5))
+        val accuracy = AASeriesElement()
+            .name("accuracy")
+            .data(arrayOf(
+                7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6
             ))
-        binding.lineChart.aa_drawChartWithChartModel(lineChartModel)
+
+        val wpm = AASeriesElement()
+            .name("WPM")
+            .data(arrayOf(
+                0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5
+            ))
+
+        val lineChartModel = AAOptions()
+            .chart(
+                AAChart()
+                    .type(AAChartType.Line)
+                    .scrollablePlotArea(AAScrollablePlotArea()
+                        .minWidth(500))
+            )
+            .title(
+                AATitle()
+                .text("Speed and accuracy"))
+            .xAxis(AAXAxis()
+                .type("category"))
+            .series(arrayOf(accuracy, wpm))
+
+        binding.lineChart.aa_drawChartWithChartOptions(lineChartModel)
 
 
 
