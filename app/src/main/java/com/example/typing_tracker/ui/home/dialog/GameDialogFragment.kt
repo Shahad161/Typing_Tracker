e12b178c6e9f42c6ecd0fa2358ed63cece009149
Shahad161 <shahad.hamza.014@gmail.com>
2021-11-27 18:00:00 +0300

    feat: nav from dialog to game and statistics.

--- app/src/main/java/com/example/typing_tracker/ui/home/dialog/GameDialogFragment.kt
+++ app/src/main/java/com/example/typing_tracker/ui/home/dialog/GameDialogFragment.kt
@@ -1,9 +1,11 @@
 package com.example.typing_tracker.ui.home.dialog
 
+import androidx.navigation.fragment.findNavController
 import com.example.typing_tracker.R
-import com.example.typing_tracker.databinding.FragmentAccuracyBinding
 import com.example.typing_tracker.databinding.FragmentGameDialogBinding
 import com.example.typing_tracker.ui.base.BaseFragment
+import com.example.typing_tracker.util.Difficulty
+import com.example.typing_tracker.util.observeEvent
 
 
 class GameDialogFragment: BaseFragment<FragmentGameDialogBinding, GameDialogModelView>() {
@@ -12,8 +14,25 @@ class GameDialogFragment: BaseFragment<FragmentGameDialogBinding, GameDialogMode
     override val viewModelClass = GameDialogModelView::class.java
 
     override fun observeEvents() {
+        viewModel.navigateToStatistics.observeEvent(this){
+            navigateToStatistics()
+        }
+
+        viewModel.navigateToGame.observeEvent(this){
+            navigateToGame()
+        }
     }
 
     override fun setUpBinding() {
     }
+
+    private fun navigateToGame() {
+        findNavController().navigate(GameDialogFragmentDirections.actionGameDialogFragmentToHomeFragment(Difficulty.EASY))
+    }
+
+    private fun navigateToStatistics() {
+        val action = GameDialogFragmentDirections.actionGameDialogFragmentToStatisticsFragment()
+        findNavController().navigate(action)
+    }
+
 }
--- app/src/main/java/com/example/typing_tracker/ui/home/dialog/GameDialogModelView.kt
+++ app/src/main/java/com/example/typing_tracker/ui/home/dialog/GameDialogModelView.kt
@@ -1,6 +1,28 @@
 package com.example.typing_tracker.ui.home.dialog
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.typing_tracker.ui.base.BaseViewModel
+import com.example.typing_tracker.util.Event
 
 class GameDialogModelView: BaseViewModel() {
+
+
+    private val _navigateToGame = MutableLiveData<Event<Boolean>>()
+    val navigateToGame : LiveData<Event<Boolean>> = _navigateToGame
+
+    private val _navigateToStatistics = MutableLiveData<Event<Boolean>>()
+    val navigateToStatistics: LiveData<Event<Boolean>> = _navigateToStatistics
+
+
+
+    fun onClickedAgain() {
+        _navigateToGame.postValue(Event(true))
+    }
+
+    fun onClickedStatistics(){
+        _navigateToStatistics.postValue(Event(true))
+    }
+
+
 }
