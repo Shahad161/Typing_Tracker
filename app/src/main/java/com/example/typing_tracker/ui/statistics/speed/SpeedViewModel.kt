b8fb53ddc7d5fc7afd51603a03c2c82e434fcea9
Karrar Mohammed <karrar.m.habeeb@gmail.com>
2021-11-27 06:47:25 +0300

    feat: create speed fragment

---
+++ app/src/main/java/com/example/typing_tracker/ui/statistics/speed/SpeedViewModel.kt
@@ -0,0 +1,33 @@
+package com.example.typing_tracker.ui.statistics.speed
+
+import com.example.typing_tracker.ui.base.BaseViewModel
+
+class SpeedViewModel: BaseViewModel() {
+
+    val data = arrayOf(
+        arrayOf("A", 1000),
+        arrayOf("B", 575),
+        arrayOf("C", 523),
+        arrayOf("D", 427),
+        arrayOf("E", 399),
+        arrayOf("F", 309),
+        arrayOf("G", 278),
+        arrayOf("H", 239),
+        arrayOf("I", 235),
+        arrayOf("J", 203),
+        arrayOf("K", 182),
+        arrayOf("L", 157),
+        arrayOf("M", 149),
+        arrayOf("N", 144),
+        arrayOf("O", 143),
+        arrayOf("P", 137),
+        arrayOf("Q", 134),
+        arrayOf("R", 118),
+        arrayOf("S", 118),
+        arrayOf("T", 117)
+    )
+
+    val speedData = arrayOf(
+        0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5
+    )
+}
