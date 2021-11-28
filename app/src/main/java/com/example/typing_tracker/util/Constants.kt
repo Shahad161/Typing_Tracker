aa050a1378cb60411976a684042cdf206e4880c2
Shahad161 <shahad.hamza.014@gmail.com>
2021-11-28 17:27:54 +0300

    change char color

--- app/src/main/java/com/example/typing_tracker/util/Constants.kt
+++ app/src/main/java/com/example/typing_tracker/util/Constants.kt
@@ -3,7 +3,7 @@ package com.example.typing_tracker.util
 object Constants {
 
     const val LOG_TAG= "TYPING_TRACKER"
-    const val CORRECT_CHAR_COLOR ="green"
+    const val CORRECT_CHAR_COLOR ="#008CFF"
     const val WRONG_CHAR_COLOR ="red"
     const val NEXT_CHAR_COLOR ="#D6DBDF"
     val difficulty = listOf("Easy", "Normal", "Hard")
